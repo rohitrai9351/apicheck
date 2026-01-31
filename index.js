dfc55834f680887efdec229f21d069109c37843b
Rohit kumar <86903609+rohitrai9351@users.noreply.github.com>
2026-01-31 06:11:02 +0000

    vercel modificiation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express()
 const {initializeDatabase} = require('./database/database.connecr');
 app.use(express.json());
+app.use(cors())
 const Book = require('./models/book.models')
 
 //q1
